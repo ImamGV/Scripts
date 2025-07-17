2d0d1a8de1286e2c2ebefc3567d4b60635f65431
ImamGV <youhubdoors@gmail.com>
2025-07-17 10:36:47 +0700

    Create FSK_Setting.lua

---
+++ FSK_Setting.lua
@@ -0,0 +1,45 @@
+local Setting = {
+  AutoGenerator = {
+    Delay = 3,
+    Enabled = true
+  },
+  AutoEatPizza = {
+    ["Health Used"] = 50,
+    Enabled = true
+  },
+  AutoCoinFlip = {
+    ["Flip Delay"] = 0.5,
+    Enabled = true
+  },
+  HideBar = {
+    Enabled = false
+  },
+  Player = {
+    Speed = {
+      ["Speed Boost"] = 0.2,
+      Enabled = false
+    },
+    InfStamina = {
+      Enabled = true
+    },
+  },
+  Anti = {
+    JohnDoeTrail = {
+      Enabled = false
+    },
+    JohnDoeTrap = {
+      Enabled = false
+    },
+  },
+  Camera = {
+    FieldOfView = {
+      ["Field Of View"] = 120,
+      Enabled = false
+    },
+  },
+  Lighting = {
+    ["No Fog"] = false,
+    ["Fullbright"] = false
+  }
+}
+  
